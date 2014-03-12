07ad3f81bfaa0a580a49c50a1b1d034d11b0c77f
rajivprith <rajiv.prithvi@gmail.com>
2014-03-12 12:26:11 -0700

    Perl_code_to_process_filepath
    The descriptions are indicated in the code

---
+++ process_filepath.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+
+#Perl code to process filepath/directory path 
+
+my $bldName = "";
+my $fpath = "/export/home/workspace/nightly.fusion.main/fusion/components/services/configMgr/csm/cmNetCfg.cc";
+
+#======================
+# The following code strips a file-path/location into a filename &
+# dierctory path to the file. 
+#======================
+print "\nFilePath => " . $fpath . "\n";
+ 
+my($filename, $directories) = fileparse($fpath);
+print "Filename  : " . $filename    . "\n";
+print "Directory : " . $directories . "\n";
+=cut
+
+#======================
+# The following code splits the directory-path between "/" and
+# assigns into an array. We search through the array to find a 
+# keyword that matches a specific directory
+#======================
+my @filecomp = split(/\//,$fpath);
+#my $size = @filecomp;
+#print "$size\n \n";
+
+for (my $i = 0; $i < @filecomp; $i++) 
+{
+=put 
+ if($i)
+	{
+		print "#". $i.":".$filecomp[$i] . "\n";
+	}
+=cut		
+	if ($filecomp[$i] eq "workspace")
+	{
+		$bldName = $filecomp[$i+1];
+		last;
+	}
+}
+print "\nString value = ". $bldName . "\n";
+
